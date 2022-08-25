9cb91dede849a80f2fb57697c87bccc73a496354
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-08-25 17:19:17 -0300

    primeiros comandos com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://i.imgur.com/zoIkaCY.jpeg" alt="Cachorro fofo demais" className='img' />
+        </>
+    );
+}
+
+export default Home;
