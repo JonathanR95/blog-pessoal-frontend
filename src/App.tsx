e05ea12416df03058f07659c0f0feb4024f694f0
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-08-30 18:20:16 -0300

    criando pagina de Login e o sistema de routas do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/static/navbar/Navbar'
 import Footer from './components/static/footer/Footer'
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
+
 
 
 function App() {
   return (
-  <>
+  <Router>
+
     <Navbar />
-    <Home />
+
+        <Routes> {/* Switch */}
+        
+  
+          <Route path='/' element={<Login/>} />
+
+          <Route path='/login' element={<Login/>} />
+
+          <Route path='/home' element={<Home />} />
+    
+
+      </Routes>
+
     <Footer />
-  </>
+
+  </Router>
   );
 };
 
--- src/components/static/navbar/Navbar.tsx
+++ src/components/static/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import {Link} from 'react-router-dom'
+import './Navbar.css'
 import {Box} from "@mui/material"
 
 function Navbar(){
@@ -34,11 +36,14 @@ function Navbar(){
                                 cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1} style={{ cursor: "pointer", color:"white" }}>
                             <Typography variant="h6" color="inherit">
                                 logout
                             </Typography>
                         </Box>
+                        </Link>
+                        
                     </Box>
 
                 </Toolbar>
